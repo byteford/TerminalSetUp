2b447719b020e06133af42d05e1871976cd2bd02
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 11:49:09 +0100

    changed mv to cp

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -4,5 +4,5 @@ apt-get install -y vim
 apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-mv .zshrc ~/.zshrc
-mv .p10k.zsh ~/.p10k.zsh
+cp .zshrc ~/.zshrc
+cp .p10k.zsh ~/.p10k.zsh
