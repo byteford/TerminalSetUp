0c2c736e0df8b1bab85b0c50c17374662cc87775
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 11:39:42 +0100

    powerlevel10k install

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -3,4 +3,5 @@ apt-get install -y curl
 apt-get install -y vim
 apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 mv .zshrc ~/.zshrc
