f8513b8ff319bd9b4dcb707de2e77c6176900f8a
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 10:39:11 +0100

    changed ohmyzsh to unatended mode

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -2,4 +2,5 @@ apt-get update
 apt-get install -y curl
 apt-get install -y vim
 apt-get install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
