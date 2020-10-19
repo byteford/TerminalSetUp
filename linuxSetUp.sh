0b6eb34fb6ae02ce35d99429c3a6b1807070378e
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 12:26:49 +0100

    added set up for tmux

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -1,7 +1,9 @@
 apt-get update
+apt update
 apt-get install -y curl
 apt-get install -y vim
 apt-get install -y zsh
+apt install -y tmux
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -10,3 +12,5 @@ cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
 cp .vimrc ~/.vimrc
 vim +PlugInstall +qall
+
+cp .tmux.conf ~/.tmux.conf
