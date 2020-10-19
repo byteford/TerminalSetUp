1fed0b27217a57842f3d11a55dea90f550216b5d
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 11:53:43 +0100

    vim now auto installs plug

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -6,3 +6,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
+cp .vimrc ~/.vimrc
