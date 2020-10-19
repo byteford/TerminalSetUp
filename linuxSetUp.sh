2a39dc2687bfdb0ee132cf12451dd8562f599b62
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 15:02:52 +0100

    added end line

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -2,8 +2,8 @@ divider===============================
 divider=$divider$divider
 width=20
 
-printf "%$width.${width}sStart%$width.${width}s\n " "$divider" "$divider"
-printf "%$width.${width}sInstall Software%$width.${width}s\n " "$divider" "$divider"
+printf "%$width.${width}sStart%$width.${width}s\n" "$divider" "$divider"
+printf "%$width.${width}sInstall Software%$width.${width}s\n" "$divider" "$divider"
 apt-get update
 apt update
 apt-get install -y curl
@@ -13,3 +13,4 @@ apt install -y tmux
 printf "%$width.${width}sFinished Installing Software%$width.${width}s\n " "$divider" "$divider"
 source ./plugins.sh
 source ./configcopy.sh
+printf "%$width.${width}sFinished%$width.${width}s\n " "$divider" "$divider"
