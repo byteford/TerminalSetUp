baab49aada5e9ea77181a297a3adff762fad0158
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 12:03:48 +0100

    added zsh-syntax-highlighting

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -4,6 +4,8 @@ apt-get install -y vim
 apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
 cp .vimrc ~/.vimrc
