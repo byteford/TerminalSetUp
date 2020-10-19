7b56ce5f56a983072be6aefa39e6777d726ce950
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 14:28:10 +0100

    removed un needed log

--- plugins.sh
+++ plugins.sh
@@ -1,3 +1,3 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh >>plugin.log)" "" --unattended >> plugin.log
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended >> plugin.log
 git clone --quiet --depth=1 https://github.com/romkatv/powerlevel10k.git  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k >> plugin.log
 git clone --quiet --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting >> plugin.log
