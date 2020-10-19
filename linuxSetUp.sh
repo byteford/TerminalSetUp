48e5aad297461e14ff51e5ee6d08744b542de811
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 10:31:48 +0100

    removed the zsh command

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -2,5 +2,4 @@ apt-get update
 apt-get install -y curl
 apt-get install -y vim
 apt-get install -y zsh
-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
