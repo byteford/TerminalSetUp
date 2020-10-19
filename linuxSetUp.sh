a0d15d2a6de806c8addb65adb888ff0909e31b5a
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 11:00:40 +0100

    move zshfile

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -3,4 +3,4 @@ apt-get install -y curl
 apt-get install -y vim
 apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
+mv .zshrc ~/.zshrc
