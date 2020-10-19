531a52bc06620b2ef45028ef50218ffb155c5ddc
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-19 10:22:33 +0100

    adding vim and curl

--- linuxSetUp.sh
+++ linuxSetUp.sh
@@ -1,4 +1,6 @@
 apt-get update
+apt-get install -y curl
+apt-get install -y vim
 apt-get install -y zsh
 zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
