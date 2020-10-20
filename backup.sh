e5634814e71f10440e548610dd524e2a462175f8
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-20 14:38:31 +0100

    added global mac .gitignore

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@ cp ~/.zshrc ./.zshrc
 cp ~/.p10k.zsh ./.p10k.zsh
 cp ~/.tmux.conf ./.tmux.conf
 cp ~/.vimrc ./.vimrc
+cp ~/.gitignore ./.gitignore
 cp -r ~/.config/tmuxinator/ ./tmuxinator/
 
 ls -la
